ede6b9a2e1061834faeac35250c95097ddea9876
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-18 23:28:25 +0200

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { App as KonstaApp } from 'konsta/react'
 
 import './App.css'
