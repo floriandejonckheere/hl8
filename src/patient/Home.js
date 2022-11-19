194060c72d9b94b9c45e438ef58d5cf3deeaae4f
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-19 03:15:20 +0200

    Add request card

--- src/patient/Home.js
+++ src/patient/Home.js
@@ -5,11 +5,37 @@ import Greeting from '../Greeting'
 
 import { patients } from '../data'
 
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from '@material-tailwind/react'
+
 export default function Home() {
   return (
     <>
       <div className="p-8 flex flex-col">
         <Greeting name={patients[0].name} />
+
+        <Card className="w-full mb-3">
+          <CardBody>
+            <span className="float-right text-gray-400 text-sm">Date here</span>
+            <strong>Professional</strong> wants to
+            <ul className="mt-4">
+              <li>Read healthcare data</li>
+              <li>Read medication prescriptions</li>
+            </ul>
+
+            <p className="mt-4">Duration: 5 days</p>
+          </CardBody>
+          <CardFooter divider className="flex items-center justify-between py-3">
+            <Button color="green">Approve</Button>
+            <Button color="red">Reject</Button>
+          </CardFooter>
+        </Card>
       </div>
 
       <Navbar />
