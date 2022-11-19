5f00a6288a5807d9fb11c3d152d7036454c498f8
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-19 08:34:09 +0200

    Add blood pressure chart

--- src/patient/Data.js
+++ src/patient/Data.js
@@ -10,6 +10,8 @@ export default function Data() {
     <>
       <div className="p-8 flex flex-col">
         <Greeting name={Backend.patients[0].name} />
+
+        <img src="/images/blood_pressure_chart.svg" alt="Blood Pressure" />
       </div>
 
       <Navbar />
